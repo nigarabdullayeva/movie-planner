178bc0f96e9e59b98bb53fbaae1200c2a6b5a212
Nigar Abdullayeva <nigar_abdullaeva@hotmail.com>
2020-07-30 20:51:33 -0500

    creating first files

---
+++ schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS movie_planner_db;
+
+CREATE DATABASE movie_planner_db;
+USE movie_planner_db;
+
+CREATE TABLE movies (
+  id int NOT NULL AUTO_INCREMENT,
+  movie varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO movies (movie) VALUES ('Rush Hour 2');
