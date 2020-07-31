35759ed2d278c290ea3d945c1aefb89773de3526
Nigar Abdullayeva <nigar_abdullaeva@hotmail.com>
2020-07-30 21:03:52 -0500

    adding code main and index

--- server.js
+++ server.js
@@ -33,6 +33,23 @@ connection.connect(function(err) {
 });
 
 
+//ROUTES 
+
+
+app.get("/", (req, res) => {
+  connection.query("SELECT * FROM movies", (err, data) => {
+    res.render("index", { movies: data })
+  })
+})
+
+
+
+
+
+
+
+
+
 app.listen(PORT, function() {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
